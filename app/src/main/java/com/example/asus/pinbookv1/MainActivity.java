da0b51eb7318e27eed250ebc44face9c7592aa96
ASUS <topaloglun@itu.edu.tr>
2017-11-19 19:09:43 +0300

    This is my first commit for pinBookV1

---
+++ app/src/main/java/com/example/asus/pinbookv1/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.asus.pinbookv1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Hide the status bar.
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        // User authorization and verification controls.
+        firebaseAuth = FirebaseAuth.getInstance();
+        Intent intent = new Intent();
+
+        if (firebaseAuth.getCurrentUser() != null) {
+
+            FirebaseUser user = firebaseAuth.getCurrentUser();
+            if (user.isEmailVerified()) {
+                intent.setClass(getApplicationContext(), ProfileActivity.class);
+            } else {
+                intent.setClass(getApplicationContext(), LoginActivity.class);
+            }
+        } else {
+            intent.setClass(getApplicationContext(), EntranceActivity.class);
+        }
+
+        // Pass to the next page.
+        passToNextActivity(intent);
+
+    }
+
+    private void passToNextActivity(Intent intent) {
+        finish();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //
+            }
+        }, 300000);
+
+        startActivity(intent);
+
+
+    }
+}
